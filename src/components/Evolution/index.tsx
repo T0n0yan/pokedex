0064ea5dbfc952ed9e9e793045064d78048a6982
Davit <t0n0yan@internet.ru>
2023-07-15 14:05:27 +0400

    last update with styles and responsive

--- src/components/Evolution/index.tsx
+++ src/components/Evolution/index.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useEffect, useState } from 'react';
-import axios from 'axios';
 import { Pokemon, SinglePokemonData } from 'store/reducers/types';
-import CardComponent from 'components/Card';
+import React, { FC, useEffect, useState } from 'react';
+import { RightOutlined } from '@ant-design/icons';
 import styles from './Evolution.module.scss';
+import CardComponent from 'components/Card';
+import axios from 'axios';
 
 type TEvolutionProps = {
   url?: string; // Species ID to get its evolved form data
@@ -48,7 +49,12 @@ const Evolution: FC<TEvolutionProps> = ({ url }) => {
         {pokemonDataState.map((pokemon, index) => {
           return (
             <div key={index} className={styles.evolution_item}>
-              <CardComponent url={pokemon.varieties[0].pokemon.url} name={pokemon.varieties[0].pokemon.name} />
+              <div>
+                <CardComponent url={pokemon.varieties[0].pokemon.url} name={pokemon.varieties[0].pokemon.name} />
+              </div>
+              <div className={styles.arrow_cont}>
+                <RightOutlined className={styles.arrow} />
+              </div>
             </div>
           );
         })}
